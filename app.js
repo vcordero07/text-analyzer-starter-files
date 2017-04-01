3c536201500a5480bbc63f93f5ddedccd79b046c
vcordero07 <vcordero07@gmail.com>
2017-04-01 18:31:20 -0400

    Added wordCount, wordFrequencie, and averageWordLength

--- app.js
+++ app.js
@@ -8,3 +8,52 @@
 //get unique word count
 //get average word length
 //
+let strTxt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna lorem ipsum dolor eiusmod elit magna aliqua.";
+
+
+let getWords = strRaw => {
+    //console.log(str.replace(/[\.,:-]+/g, '').split(' '));
+    return strRaw.replace(/[\.,:-]+/g, '').split(' ').sort();
+};
+
+
+
+let getWordsTotal = str => {
+    // console.log(str);
+    let result;
+    let words = getWords(str);
+    let wordAvg = 0;
+    let wordCount = words.length;
+    let wordFrequencies = {};
+
+
+    //get wordFrequencies
+    //console.log(words);
+    for (let i = 0; i <= words.length; i++) {
+        if (words[i] in wordFrequencies) {
+            wordFrequencies[words[i]]++;
+        } else {
+            wordFrequencies[words[i]] = 1;
+        }
+    }
+    //console.log(wordFrequencies);
+
+
+
+
+    //get the average word length
+    //console.log(wordCount);
+    for (let i = 0; i < wordCount; i++) {
+        wordAvg += words[i].length;
+    }
+    let avgLen = wordAvg / wordCount;
+    //console.log(avgLen);
+
+
+
+
+    return result;
+
+};
+
+getWordsTotal(strTxt);
